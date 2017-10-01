9a92145ed7f8c4502cd6f3a17d17245f91185889
Mohamad Barbar <mbarbar@runbox.com>
2017-10-01 22:47:49 +1100

    Add lpcfi skeleton.

---
+++ lpcfi.c
@@ -0,0 +1,26 @@
+/** lpcfi.c - implementation of interface in lpcfi.h. */
+
+void lpcfi_init(void) {
+
+}
+
+void lpcfi_destroy(void) {
+
+}
+
+void lpcfi_set_const(char **ptr, char *val) {
+
+}
+
+void lpcfi_set_dyn(char **ptr, char **deref_ptr) {
+
+}
+
+int lpcfi_check_ptr(char **ptr) {
+
+}
+
+int lpcfi_within(char **ptr, unsigned int n_ptrs, ...) {
+
+}
+
