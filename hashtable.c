142d1b4a259bb423618660d799bb328d697ec250
Mohamad Barbar <mbarbar@runbox.com>
2017-10-01 21:13:33 +1100

    Add hashtable structure + init + destroy.

--- hashtable.c
+++ hashtable.c
@@ -4,15 +4,35 @@
 
 #include "hashtable.h"
 
+struct hashnode {
+        char **key;
+        char *val;
+        struct hashnode *next;
+};
+
 struct hashtable {
+        size_t max_size;  /** Number of keys the table can take. */
+        size_t n_keys;    /** Number of keys currently in the table. */
+        struct hashnode **table;  /** Array of hashnodes. */
 };
 
 hashtable ht_init(size_t size) {
-        return NULL;
+        hashtable ht = malloc(sizeof(struct hashtable));
+        if (ht == NULL) return NULL;
+
+        ht->table = malloc(sizeof(struct hashnode *) * size);
+        if (ht->table == NULL) {
+                free(ht);
+                return NULL;
+        }
+
+        return ht;
 }
 
 void ht_destroy(hashtable ht) {
-
+        if (ht == NULL) return;
+        free(ht->table);
+        free(ht);
 }
 
 int ht_lookup(hashtable ht, char **key, char **val) {
