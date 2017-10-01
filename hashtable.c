f468035312d7de130dc62b0fff65c49426284875
Mohamad Barbar <mbarbar@runbox.com>
2017-10-01 20:45:51 +1100

    Add hashtable skeleton.

---
+++ hashtable.c
@@ -0,0 +1,26 @@
+/** hashtable.c - implementation of interface in hashtable.h. */
+
+#include <stdlib.h>
+
+#include "hashtable.h"
+
+struct hashtable {
+};
+
+hashtable ht_init(size_t size) {
+        return NULL;
+}
+
+void ht_destroy(hashtable ht) {
+
+}
+
+int ht_lookup(hashtable ht, char **key, char **val) {
+
+        return 0;
+}
+
+void ht_set(hashtable ht, char **key, char *val) {
+
+}
+
