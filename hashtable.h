5e5965dc42e92d956e61ff18f7169a3db7b0cab3
Mohamad Barbar <mbarbar@runbox.com>
2017-10-01 20:43:39 +1100

    Fix hashtable interface + doc.

--- hashtable.h
+++ hashtable.h
@@ -1,5 +1,5 @@
-/** hashtable.h - interface to a hash table with pointer keys and
- *                pointer values.
+/** hashtable.h - interface to a hash table with pointer keys (address
+ *                of function pointers) and pointer values.
  */
 
 #ifndef HASHTABLE_H
@@ -14,13 +14,13 @@ hashtable ht_init(size_t size);
 void ht_destroy(hashtable ht);
 
 /** Looks up the value associated with key in ht. If there is a
- *  value associated with key, returns a true value, and sets val
+ *  value associated with key, returns a true value, and sets *val
  *  to that value. Otherwise, returns 0 and leaves val untouched.
  */
-int ht_lookup(hashtable ht, char *key, char **val);
+int ht_lookup(hashtable ht, char **key, char **val);
 
 /** Sets key to val in ht. */
-void ht_set(hashtable ht, char *key, char *val);
+void ht_set(hashtable ht, char **key, char *val);
 
 #endif  /* HASHTABLE_H */
 
